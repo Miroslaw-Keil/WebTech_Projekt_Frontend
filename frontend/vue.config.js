480af69cc73e992f1e3112242f31bf73c5395b9f
Miroslaw-Keil <Miroslaw.Keil@Student.HTW-Berlin.de>
2022-12-14 14:57:49 +0100

    Github Page

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,5 +1,6 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  devServer: {port: "3000"}
+  devServer: {port: "3000"},
+  publicPath: '/Webtech_Projekt_Frontend/'
 })
