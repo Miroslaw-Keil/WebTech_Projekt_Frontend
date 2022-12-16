f5db5d985624bd17e488580aac8ac25a9d6681d9
Miroslaw-Keil <Miroslaw.Keil@Student.HTW-Berlin.de>
2022-12-16 20:49:31 +0100

    Umstrukturierung

---
+++ src/views/AlleEinsehenView.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="extractAll">
+    <h1>Das ist die Seite um alles anzeigen zu lassen.</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AlleEinsehenView'
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="home">
+    <h1>Das ist die Homepage</h1>
+    <img alt="Bold and Brash" src="../assets/bold_and_brash.png" width="600" height="400">
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'HomeView',
+  components: {
+    // eslint-disable-next-line vue/no-unused-components
+    HelloWorld
+  }
+}
+</script>
