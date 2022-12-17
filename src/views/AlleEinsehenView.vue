be01f31399827dd6afa260c8a909c8aca9bc6f51
Miroslaw-Keil <Miroslaw.Keil@Student.HTW-Berlin.de>
2022-12-17 15:31:51 +0100

    HTML und Verknüpfung mit dem Backend

--- src/views/AlleEinsehenView.vue
+++ src/views/AlleEinsehenView.vue
@@ -1,12 +1,76 @@
 <template>
+  <div><h1>Alle Gerichte ausgeben.</h1></div>
   <div class="extractAll">
-    <h1>Das ist die Seite um alles anzeigen zu lassen.</h1>
+<!--    <input v-model="gerichtID" placeholder="ID">-->
+<!--    <input v-model="name" placeholder="Name">-->
+<!--    <input v-model="tageszeit" placeholder="Tageszeit">-->
+<!--    <input v-model="vegan" placeholder="Vegan">-->
+<!--    <input v-model="zubereitungsdauer" placeholder="Zubereitungsdauer">-->
+  </div>
+  <div>
+    <table class="table table-striped">
+      <thead>
+      <tr>
+      <th>Name</th>
+      <th>Tageszeit</th>
+      <th>Vegan</th>
+      <th>Zubereitungsdauer</th>
+      </tr>
+      </thead>
+      <tbody>
+      <tr v-if="gerichte.length === 0">
+      </tr>
+      <tr v-for="gericht in gerichte" :key="gericht.id">
+        <td>{{gericht.name}}</td>
+        <td>{{gericht.tageszeit}}</td>
+        <td>{{gericht.vegan}}</td>
+        <td>{{gericht.zubereitungsdauer}}</td>
+      </tr>
+      </tbody>
+    </table>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'AlleEinsehenView'
+  name: 'AlleEinsehenView',
+
+  data() {
+    return {
+      gerichte: []
+    }
+  },
+
+  methods: {
+    loadGerichte () {
+      const endpoint = 'http://localhost:8080/api/v1/gerichte'
+      const requestOptions = {
+        method: 'GET',
+        redirect: 'follow'
+      }
+      fetch(endpoint, requestOptions)
+        .then(response => response.json)
+        .then(result => result.forEach(gericht => {
+          this.items.push(gericht)
+        }))
+        .catch(error => console.log('error', error))
+    }
+  },
+
+  mounted() {
+    const endpoint = 'http://localhost:8080/api/v1/gerichte'
+    const requestOptions = {
+      method: 'GET',
+      redirect: 'follow'
+    }
+
+    fetch(endpoint, requestOptions)
+      .then(response => response.json())
+      .then(result => result.forEach(gericht => {
+        this.gerichte.push(gericht)
+      }))
+      .catch(error => console.log('error', error))
+  }
 }
 </script>
 
