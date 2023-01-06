ba0fb9a32f228078057e8f18bdb7c9d077fa6373
dalbongy <qhvien@gmail.com>
2023-01-06 22:28:38 +0100

    Okta update

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -6,7 +6,7 @@
 
 <script>
 import OktaSignIn from '@okta/okta-signin-widget'
-//import '@okta/okta-signin-widget/dist/css/okta-sign-in.min.css'
+import '@okta/okta-signin-widget/dist/css/okta-sign-in.min.css'
 
 
 import sampleConfig from '../config'
